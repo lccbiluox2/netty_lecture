0ac1bbf5c7a2c52efad2cdbf25346508edff2971
lcc <541711153@qq.com>
2019-08-13 18:56:49 +0800

    未发热个人

--- src/main/java/com/shengsiyuan/netty/firstexample/TestServer.java
+++ src/main/java/com/shengsiyuan/netty/firstexample/TestServer.java
@@ -9,6 +9,31 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class TestServer {
 
+    /**
+     * 测试方法：
+     * 1. 启动服务器
+     *
+     * 2.执行命令
+     * lcc@lcc netty_lecture$ curl 'http://localhost:8899'
+     * Hello World
+     * lcc@lcc netty_lecture$
+     *
+     * 服务器打印如下：
+     * handler added
+     * channel registered
+     * channel active
+     * class io.netty.handler.codec.http.DefaultHttpRequest
+     * /0:0:0:0:0:0:0:1:59383
+     * 请求方法名：GET
+     * class io.netty.handler.codec.http.LastHttpContent$1
+     * /0:0:0:0:0:0:0:1:59383
+     * channel inactive
+     * channel unregistered
+     *
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
